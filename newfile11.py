60bc0108fe25f37ae5f0192757494e2ae4c8d707
𝕽𝖆𝖍𝖎𝖒🥶 <sh31khrxh1m3@gmail.com>
2025-10-05 12:35:00 +0100

    Update newfile11.py

--- newfile11.py
+++ newfile11.py
@@ -8,3 +8,18 @@ filename = 'Assets/yolo3.txt'
 with open(filename,'rt') as fpt:
     classLabels = fpt.read().rstrip('\n').split('\n')
 print(classLabels)
+
+final.setInputSize(320,320)
+final.setInputScale(1.0/127.5)
+final.setInputMean((127.5,127.5,127.5))
+final.setInputSwapRB(True)
+sample = cv2.imread("Assets/download.jpeg")
+index,confidence,bbox = final.detect(sample,confThreshold=0.5)
+print(confidence,index)
+
+for i,c,b in zip(index.flatten(),confidence.flatten(),bbox):
+    cv2.rectangle(sample,b,(255,0,0),2)
+    cv2.putText(sample,classLabels[i-1],(b[0]+40,b[1]+40),cv2.FONT_HERSHEY_PLAIN,2,(0,0,0),1)
+
+cv2.imshow('final',sample)
+cv2.waitKey(0)
